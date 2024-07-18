3e682b000d228a269c12a39565049d74a78d3357
ymohit1603 <mohityadav0330@gmail.com>
2024-07-18 22:09:58 +0530

    Add: added feedCard

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -1,30 +1,67 @@
+import Image from "next/image";
 import React from "react";
-import { AiOutlineHeart } from "react-icons/ai";
-import { BiMessageRounded, BiUpload } from "react-icons/bi";
-import {FaRetweet} from 'react-icons/fa'
-const FeedCard:React.FC=()=>{
-    return <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-4 hover:bg-slate-900 transistion-all cursor-pointer">
-        <div className="grid grid-cols-12">
-            <div className="col-span-1"></div>
-            <div className="col-span-11">
-                <div className="justify-between mt-5 text-center-xl items-center">
-                    <div>
-                        <BiMessageRounded/>
-                    </div>
-                    <div>
-                        <FaRetweet/>
-                    </div>
-                    <div>
-                        <AiOutlineHeart/>
-                    </div>
-                    <div>
-                        <BiUpload/>
-                    </div>
-                </div>
-            </div>
+import { BiMessageRounded } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+import { CgPoll } from "react-icons/cg";
+import { FiMoreHorizontal, FiUpload } from "react-icons/fi";
+import { Likes, Tweet } from "@/gql/graphql";
+import Link from "next/link";
+import LikeButton from "./LikeButton";
 
-        </div>
-    </div>
+interface feedCardProps {
+  tweet: Tweet;
 }
+
+const FeedCard: React.FC<feedCardProps> = ({ tweet }) => {
+  return (
+    <div className="grid grid-cols-12 gap-3 border-b-[0.5px] border-gray-800 p-4 transition-all hover:bg-gray-900/20">
+      {tweet.author?.profileImgUrl && (
+        <Image
+          src={tweet.author.profileImgUrl}
+          width={50}
+          height={50}
+          alt="avatar"
+          className="col-span-1 rounded-full"
+        />
+      )}
+      <div className="col-span-11 ">
+        <span className="float-right py-1 text-gray-600">
+          <FiMoreHorizontal />
+        </span>
+        <Link href={`/${tweet.author?.id}`}>
+          <span className="font-semibold">
+            {tweet.author?.firstName} {tweet.author?.lastName}
+          </span>
+        </Link>
+        <span className="mx-3 text-gray-600">@debarkamondal</span>
+        <div className="my-1">{tweet.content}</div>
+        {tweet.imgUrl && (
+          <Image
+            src={tweet.imgUrl}
+            alt="tweet image"
+            height={200}
+            width={300}
+          />
+        )}
+      </div>
+      <div className="col-span-11 col-start-2 flex items-center justify-between text-xl text-gray-600">
+        <span className="flex items-center gap-2">
+          <BiMessageRounded />
+          <span className="text-sm">652</span>
+        </span>
+        <span className="flex items-center gap-2">
+          <FaRetweet />
+          <span className="text-sm">652</span>
+        </span>
+        <LikeButton tweetId={tweet.id} likes={tweet.likes as Likes[]} />
+        <span className="flex items-center gap-2">
+          <CgPoll />
+          <span className="text-sm">652</span>
+        </span>
+        <FiUpload />
+      </div>
+    </div>
+  );
+};
 
 export default FeedCard;
